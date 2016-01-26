cda008f64d526d59ebc9f02d5a8e1925b2b014c9
Alexandr Ochkur <webshulz1987@gmail.com>
2016-01-26 22:22:45 +0200

    PHP1 Home Work include Models

--- models/Models.php
+++ models/Models.php
@@ -16,40 +16,19 @@ abstract class Models
     }
 
     public static function findAll(){
-        $dbh = new Connection();
         $sql = 'SELECT * FROM ' . static::TABLE . ' ORDER BY id ASC';
+        $dbh = new Connection();
         $sth = $dbh->prepare($sql);
         $sth->execute();
         return $sth->fetchAll(PDO::FETCH_CLASS, static::class);
     }
 
 
-    public function updateById($text,$content,$id){
-        $this->id = $id;
-        $this->text = $text;
-        $this->content = $content;
-        $dbh = new Connection();
-        $sql = "UPDATE PHP1HW SET text='$text', content='$content' WHERE id='$id'";
-        $sth = $dbh->prepare($sql);
-        $sth->execute();
-
-    }
-
-    public function insert($content='',$text=''){
-        $this->content = $content;
-        $this->text = $text;
-        $dbh = new Connection();
-        $sql = "INSERT INTO PHP1HW(content, text) VALUES ('" . $content . "','" . $text . "')";
-        $sth = $dbh->prepare($sql);
-        $sth->execute();
-
-    }
-
     public function deleteById($id){
         $this->id = $id;
         $dbh = new Connection();
         $sql = 'DELETE FROM ' . static::TABLE .' WHERE id='.$id;
-        $sth = $this->dbh->prepare($sql);
+        $sth = $dbh->prepare($sql);
         $sth->execute();
 
     }
